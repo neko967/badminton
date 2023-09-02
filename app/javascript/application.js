53bf28b33153a13b8af720c0aff277e8e7b99af6
neko967 <113199838+neko967@users.noreply.github.com>
2023-09-02 14:57:09 +0900

    Add: MaterialUIの導入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,108 @@
-import "./components/react_hello"
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+import { Button , Box, BottomNavigation, BottomNavigationAction} from "@mui/material";
+import styled from "@emotion/styled";
+
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = createRoot(document.getElementById("root"));
+  root.render(
+    <div>
+      <App />
+    </div>
+  );
+})
+
+export default function App() {
+  return (
+    <div>
+      <Greeting name="world" />
+      <Screen />
+    </div>
+  );
+}
+
+function Greeting({ name }) {
+  return <h1>Hello, {name}</h1>;
+}
+
+function Screen() {
+  return (
+    <div>
+      <h1>screen</h1>
+      <Community />
+      <Score />
+      <Speek />
+      <Court />
+      <Menu />
+    </div>
+  );
+}
+
+function Community() {
+  return (
+    <div>
+      <h2>Community</h2>
+      <Depending />
+      <Player />
+    </div>
+  );
+}
+
+function Depending() {
+  return (
+    <h3>Depending</h3>
+  );
+}
+function Player() {
+  return (
+    <h3>Player</h3>
+  );
+}
+
+function Score() {
+  return (
+    <div>
+      <Button variant="contained">1</Button>
+      <Button variant="contained">1</Button>
+    </div>
+  );
+}
+
+function Speek() {
+  return (
+    <h2>Speek</h2>
+  );
+}
+
+function Court() {
+  return (
+    <h2>Court</h2>
+  );
+}
+
+function Menu() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <div>
+      <Box sx={{ width: 500 }}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        </BottomNavigation>
+      </Box>
+    </div>
+  );
+}
+
